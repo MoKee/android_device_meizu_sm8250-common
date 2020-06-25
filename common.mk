501d467780e827548c109ce6ae6869cb40b7bd1f
XiNGRZ <chenxingyu92@gmail.com>
2020-06-26 01:49:42 +0800

    sm8250: Import media_profiles_vendor.xml from stock
    Change-Id: Ide7a03d8c810e3df0e43f5e12c0f89627e4cf69f

--- common.mk
+++ common.mk
@@ -44,6 +44,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_vendor.xml
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
